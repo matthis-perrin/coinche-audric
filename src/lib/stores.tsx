6da2e8ae7c311885e2b63ca71c7874c1f9eb9670
audric-perrin <audric.perrin@gmail.com>
2022-08-02 21:02:41 +0200

    manque rafraichir

--- src/lib/stores.tsx
+++ src/lib/stores.tsx
@@ -20,7 +20,6 @@ export const useApp = appStore.useData;
 export const setApp = appStore.setData;
 
 export const setPlayer = (player: Player): void => {
-  console.log('setPlayer');
   const players = getPlayers();
   players.push(player);
   setPlayers(players);
